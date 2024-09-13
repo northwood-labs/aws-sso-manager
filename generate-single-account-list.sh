ca9302ba41ff0b0fbd4305e815d724a95dffd0c3
Ryan Parman <rparman@northwood-labs.com>
2024-09-13 14:58:50 -0600

    fix: Update generate-single-account-list.sh
    Signed-off-by: Ryan Parman <rparman@northwood-labs.com>

--- generate-single-account-list.sh
+++ generate-single-account-list.sh
@@ -5,9 +5,14 @@ set -euo pipefail
 source list-accounts.sh
 
 LIST="$(listaccounts)"
+MISSING="$(grep -v -f /tmp/local-list.txt /tmp/sso-list.txt | tr '[:space:]' ' ')"
 
-IFS=$'\n' read -d "\034" -r -a array <<< "${LIST}\034"
+IFS=$'\n' read -d "\034" -r -a array <<<"${LIST}\034"
 
 for account in "${array[@]}"; do
-    echo "./single-account.sh ${account/-/ }"
+    for mm in "${MISSING[@]}"; do
+        if [[ "${account}" == *"${mm}"* ]]; then
+            echo "./single-account.sh ${account}"
+        fi
+    done
 done
